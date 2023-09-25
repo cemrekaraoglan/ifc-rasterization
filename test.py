371ecb12c9929717a129855924c312f56cd8ef1b
cemrekaraoglan <cemre.karaoglan@rwth-aachen.de>
2023-09-25 16:20:13 +0200

    Polygon rasterization

--- test.py
+++ test.py
@@ -1,2 +1,10 @@
-import platform
-print(platform.architecture())
+import ifcopenshell.util.pset
+
+
+ifc_file = ifcopenshell.open(r"IFC Files\Duplex.ifc")
+elements = ifc_file.by_type("IfcBuildingElement")
+for element in elements:
+    if "concrete" in element.ObjectType.lower():
+        print(element.Name)
+    
+    #print(pset_qto.get_applicable_names("IfcMaterial"))
